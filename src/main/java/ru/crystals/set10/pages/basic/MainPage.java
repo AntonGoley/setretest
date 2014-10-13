6aabcff0d4f0d13c4936769aa98f63f384f20c13
AntonGoley <agoley@CS-PRG-WS171164.domain.local>
2014-10-13 17:41:16 +0400

    remove is swfReady tag

--- src/main/java/ru/crystals/set10/pages/basic/MainPage.java
+++ src/main/java/ru/crystals/set10/pages/basic/MainPage.java
@@ -18,7 +18,7 @@ public class MainPage extends AbstractPage{
 	
 	public MainPage(WebDriver driver) {
 		super(driver);
-		isSWFReady();
+		//isSWFReady();
 	}
 	
 	public OperDayPage openOperDay() {
