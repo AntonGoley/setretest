00311d4d2637df15cdbe157820fa0850be75cec7
AntonGoley <agoley@CS-PRG-WS171164.domain.local>
2014-10-16 16:12:49 +0400

    edit qnty and sum generation

--- src/main/java/ru/crystals/set10/utils/GoodsParser.java
+++ src/main/java/ru/crystals/set10/utils/GoodsParser.java
@@ -72,8 +72,8 @@ public class GoodsParser {
 	        } else {
 	        	qnt = random(5) + 1L;
 	        }
-		        pos.setQnty(Long.valueOf(qnt * 1000L));
-		        pos.setPriceEnd(Long.valueOf(random(1000) * 127L));
+		        pos.setQnty(Long.valueOf(qnt * 100L));
+		        pos.setPriceEnd(Long.valueOf(random(1000) * 17L));
 		        pos.setSum(Long.valueOf(qnt * pos.getPriceEnd().longValue()));
 		        summ += pos.getSum().longValue();
 		        pos.setNdsSum(Long.valueOf(Math.round(pos.getSum().longValue() * 0.2D)));
