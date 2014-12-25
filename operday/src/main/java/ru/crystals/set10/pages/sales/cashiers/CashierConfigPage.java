04856b7f23cbe64c0ab9dc0e244dd8c0b1e0e38b
unknown <agoley@CS-WS172878.domain.local>
2014-12-25 20:19:12 +0300

    delay after cashier adition

--- operday/src/main/java/ru/crystals/set10/pages/sales/cashiers/CashierConfigPage.java
+++ operday/src/main/java/ru/crystals/set10/pages/sales/cashiers/CashierConfigPage.java
@@ -3,7 +3,9 @@ package ru.crystals.set10.pages.sales.cashiers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import ru.crystals.set10.pages.basic.SalesPage;
+import ru.crystals.set10.utils.DisinsectorTools;
 import static ru.crystals.set10.utils.FlexMediator.*;
 
 public class CashierConfigPage extends SalesPage{
@@ -37,6 +39,7 @@ public class CashierConfigPage extends SalesPage{
 		typeText(getDriver(), ID_SALESSWF, LOCATOR_PASSWORD, password);
 		selectElement(getDriver(), ID_SALESSWF, LOCATOR_ROLE, role);
 		clickElement(getDriver(), ID_SALESSWF, LOCATOR_BACK_TO_CASHIERS_LIST);
+		DisinsectorTools.delay(1000);
 		log.info("Добавить нового кассира");
 		return new CashiersMainPage(getDriver());
 	}
