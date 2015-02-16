014c269cd9f43f186e8dbf88e5edcbff3e2e876e
unknown <agoley@CS-WS172878.domain.local>
2015-02-16 18:34:09 +0300

    transaction search test

--- operday/src/test/java/ru/crystals/set10/test/search/SearchTransactionsTest.java
+++ operday/src/test/java/ru/crystals/set10/test/search/SearchTransactionsTest.java
@@ -6,12 +6,14 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.crystals.pos.bank.datastruct.AuthorizationData;
 import ru.crystals.pos.bank.datastruct.BankCard;
 import ru.crystals.pos.payments.BankCardPaymentEntity;
 import ru.crystals.set10.pages.operday.searchcheck.TransactionSearchPage;
 import ru.crystals.set10.utils.CashEmulatorPayments;
+import ru.crystals.set10.utils.DisinsectorTools;
 import static ru.crystals.set10.pages.operday.searchcheck.CheckSearchPage.*;
-import static ru.crystals.set10.pages.operday.OperDayPage.SEARCH_TRANSACTIONS;
+import static ru.crystals.set10.pages.operday.searchcheck.TransactionSearchPage.*;
 
 public class SearchTransactionsTest extends SearchCheckAbstractTest{
 	
@@ -19,62 +21,111 @@ public class SearchTransactionsTest extends SearchCheckAbstractTest{
 	
 	CashEmulatorPayments payments = new CashEmulatorPayments();
 	
+	BankCard card;
+	AuthorizationData validAuth;
+	AuthorizationData invalidAuth;
+	
+	
 	@BeforeClass
 	public void navigateToTransactionsPage(){
 		transactions = searchCheck.navigatePage(TransactionSearchPage.class, SEARCH_TRANSACTIONS);
 		transactions.openFilter();
 		
 		purchase = payments.getPurchaseWithoutPayments();
-		
 		String prefix = String.valueOf(System.currentTimeMillis()).substring(5);
 		String bankCardNumber = String.format("1234****%s", prefix);
 		
-		BankCard card = payments.setBankCardData(bankCardNumber, "VISA");
-		purchase = payments.setBankCardPayment(BankCardPaymentEntity.class, purchase, purchase.getCheckSumEnd()/2, card, null);
+		/*
+		 * Формируем чек с транзакциями:
+		 *   - отклоненная банковская транзакция (сумма оплаты = сумме чека)
+		 *   - положительная транзакция (сумма оплаты = (сумма чека)/2)
+		 *   - оплата наличными (сумма оплаты = (сумма чека)/2);
+		 *   - в чеке банковская транзакция по одной банковской карте
+		 */
+		
+		validAuth = generateAuthData(true);
+		invalidAuth = generateAuthData(false);
+		
+		card = payments.setBankCardData(bankCardNumber, "VISA");
+		purchase = payments.setBankCardPayment(BankCardPaymentEntity.class, purchase, purchase.getCheckSumEnd(), card, invalidAuth);
+		purchase = payments.setBankCardPayment(BankCardPaymentEntity.class, purchase, purchase.getCheckSumEnd()/2, card, validAuth);
+		purchase = payments.setCashPayment(purchase, purchase.getCheckSumEnd() - purchase.getCheckSumEnd()/2);
 		sendCheck(purchase);
 		
 		/*
-		 * убедиться, что чек в системе
+		 * убедиться, что чек в системе и найдено 2 транзакции
 		 */
 		transactions.setCheckBarcode(purchase);
 		transactions.doSearch();
-		transactions.getExpectedResultCount(1);
+		transactions.getExpectedResultCount(2);
 	}
 	
 	@BeforeMethod
 	public void resetSearch(){
 		/*
-		 * Сбрасываем рез-т поиска, указывая баркод чека,
+		 * Сбрасываем рез-т поиска (находим 0 транзакций), указывая баркод чека,
 		 * которого нет в системе
 		 */
 		transactions.setFilterText(FILTER_CATEGORY_CHECK_BAR_CODE, "000");
 		transactions.getExpectedResultCount(0);
 	}
 	
-	@DataProvider(name = "Положительная транзакция в чеке")	
-	public Object[][] positiveTransactionCreteria(){
+	@DataProvider(name = "Количество найденных транзакций")	
+	public Object[][] transactionsCount(){
 		return new Object[][]{
-				{"Поиск транзакции по номеру чека", FILTER_CATEGORY_CHECK_NUMBER, String.valueOf(purchase.getNumber())},
-				{"Поиск транзакции по номеру кассы", FILTER_CATEGORY_CASH_NUMBER, String.valueOf(purchase.getShift().getCashNum())},	
+				{"Поиск транзакции по коду авторизации", FILTER_CATEGORY_AUTHORIZATION_CODE, validAuth.getAuthCode() , 1},
+				{"Поиск транзакции по номеру банковской карты", FILTER_CATEGORY_BANK_CARD_NUMBER, card.getCardNumber() , 2},
+				{"Поиск транзакции по номеру терминала", FILTER_CATEGORY_TERMINAL_NUMBER, validAuth.getTerminalId() , 1},
+				{"Поиск транзакции по коду ответа процессингового центра", FILTER_CATEGORY_SERVER_RESPONSE_CODE, validAuth.getResponseCode(), 1},
+//				{"Поиск транзакции по сумме оплаты отклоненной транзакции", FILTER_CATEGORY_SERVER_RESPONSE_CODE, "", 1},
+//				{"Поиск транзакции по сумме оплаты положительной транзакции", 1},
+//				{"Поиск транзакции по сумме чека", 2},
 		};
 	};
 	
-	@Test (description = "SRTE-79. Поиск чека, содержащего положительную транзакцию", 
-			dataProvider = "Положительная транзакция в чеке")
-	public void testSearchTransactions(String field, String filterLocator , String filterValue){
-
+	@DataProvider(name = "Поля найденных транзакций")	
+	public Object[][] transactionsContent(){
+		/*
+		 * Добавить hashMap для проверки полей найденных транзакций
+		 */
+		return new Object[][]{
+				{"Поиск транзакции по коду авторизации", FILTER_CATEGORY_AUTHORIZATION_CODE, ""},
+				{"Поиск транзакции по номеру банковской карты", FILTER_CATEGORY_BANK_CARD_NUMBER, ""},
+				{"Поиск транзакции по номеру терминала", FILTER_CATEGORY_TERMINAL_NUMBER, ""},
+				{"Поиск транзакции по коду ответа процессингового центра", FILTER_CATEGORY_SERVER_RESPONSE_CODE, ""},
+//				{"Поиск транзакции по сумме оплаты отклоненной транзакции", FILTER_CATEGORY_SERVER_RESPONSE_CODE},
+//				{"Поиск транзакции по сумме оплаты положительной транзакции"},
+//				{"Поиск транзакции по сумме чека"},
+		};
+	};
+	
+	@Test (description = "SRTE-79. Поиск транзакций оплаты по основным параметрам", 
+			dataProvider = "Количество найденных транзакций")
+	public void testSearchTransactions(String field, String filterLocator , String filterValue, int transactionsCount){
 		transactions.setFilterMultiText(filterLocator, filterValue);
 		transactions.doSearch();
  		
  		searchResult = searchCheck.getSearchResultCount();
  		
- 		
-		Assert.assertEquals(searchCheck.getExpectedResultCount(searchResult + 1), searchResult + 1, "");
-		
- 		testExcelExport(LOCATOR_XLS_CHECK_CONTENT, XLS_REPORT_CONTENT_PATTERN);
- 		testExcelExport(LOCATOR_XLS_CHECK_HEADERS, XLS_REPORT_HEADERS_PATTERN);
- 		
- 		
+		Assert.assertEquals(searchCheck.getExpectedResultCount(transactionsCount), transactionsCount, "Неверное число найденных трназакций оплаты");
+ 		testExcelExport(LOCATOR_XLS_TRANSACTIONS, XLS_REPORT_TRANSACTIONS);
 	}
+	
+	
+	private AuthorizationData generateAuthData(boolean transactionStatus){
+		String message = transactionStatus ?  "ОДОБРЕНО" : "ОТКЛОНЕНО";
+		
+		DisinsectorTools.delay(99);
+		AuthorizationData authData = new AuthorizationData();
+		authData.setStatus(transactionStatus);
+		authData.setBankid("ВТБ");
+		authData.setAuthCode(String.valueOf(System.currentTimeMillis()));
+		authData.setMessage(message);
+		authData.setResponseCode(String.valueOf(System.currentTimeMillis()).substring(5));
+		authData.setTerminalId("AA" + String.valueOf(System.currentTimeMillis()).substring(4));
+		authData.setResultCode(System.currentTimeMillis() - 10000000000L);
+		return authData;
+	}
+	
 	
 }
