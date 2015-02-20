38befa0ec4cf4e58df10d7ea1014e4beb6a96dab
unknown <agoley@CS-WS172878.domain.local>
2015-02-20 14:10:01 +0300

    fix for km3 after replacing check box with button

--- operday/src/main/java/ru/crystals/set10/pages/operday/cashes/KmPage.java
+++ operday/src/main/java/ru/crystals/set10/pages/operday/cashes/KmPage.java
@@ -12,7 +12,7 @@ public class  KmPage extends OperDayPage{
 	public static final String LOCATOR_KM3 = "КМ-3";
 	public static final String LOCATOR_KM6 = "КМ-6";
 	private static final String NAVIGATOR_KM_TYTE = "id:tabNav/className:Tab;label:";
-	private static final String LOCATOR_PRINTALL_CHECKBOX = "selectedAllCheckBox";
+	private static final String BUTTON_SELECT_ALL_UNPRINTED = "label:Выбрать все ненапечатанные";
 	private static final String LOCATOR_PRINTALL_BUTTON  = "label=Распечатать выбранные";
 	
 	public static final String LOCATOR_KM3_TABLE = "km3Table";
@@ -36,7 +36,7 @@ public class  KmPage extends OperDayPage{
 	}
 	
 	public String printAllKmForms(){
-		checkBoxValue(getDriver(), ID_OPERDAYSWF, LOCATOR_PRINTALL_CHECKBOX, true);
+		clickElement(getDriver(), ID_OPERDAYSWF, BUTTON_SELECT_ALL_UNPRINTED);
 		waitForProperty(getDriver(), ID_OPERDAYSWF, LOCATOR_PRINTALL_BUTTON, new String[]{"enabled", "true"});
 		clickElement(getDriver(), ID_OPERDAYSWF, LOCATOR_PRINTALL_BUTTON);
 		return getReportText();
