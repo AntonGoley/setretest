39f42edc588c1c134b72abb2d3c16dc26abbc49e
unknown <agoley@CS-WS172878.domain.local>
2015-06-30 13:56:43 +0300

    generators updates

--- operday/src/test/java/ru/crystals/set10/test/checkgenerator/OdGeneratorTest.java
+++ operday/src/test/java/ru/crystals/set10/test/checkgenerator/OdGeneratorTest.java
@@ -1,12 +1,17 @@
 package ru.crystals.set10.test.checkgenerator;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import ru.crystals.set10.test.maincash.ConfigMainCash;
 
 
 public class OdGeneratorTest {
+	
+	private static final Logger log = Logger.getLogger(OdGeneratorTest.class);
 	
 	long day = 86400000L;
 	
@@ -16,19 +21,32 @@ public class OdGeneratorTest {
 	}
 	
 	
-	//@Parameters ("operdays")
-	@Test (	description = "Генерация 3 операционных дней на позавчера, вчера, сегодня", 
+	
+	@Parameters ("operdays")
+	@Test (	description = "Генерация операционных дней на позавчера, вчера, сегодня", 
 			groups = "od_generator")
-	public void testSendChecks(/*int operdays*/){
+	public void testSendChecks(@Optional Integer operdays){
 		
+		if (operdays == null) {
+			operdays = 3;
+		};	
 		
+		log.info("В системе будет создано операционных дней: " +  operdays);
 		
-		Long[] operDays = {
-				day*2L,
-				day,
-				0L
-			};
-		ConfigMainCash.createODWithCashDocs(operDays);
+		/*сколько будет создано опердней */
+		Long[] od_range = new Long[operdays];
+		
+		for (int i = operdays; i>0; i-- ) {
+			Number n = i - 1;
+			od_range[n.intValue()] = (day * n.longValue() );
+		}
+
+//		Long[] operDays = {
+//				day*2L,
+//				day,
+//				0L
+//			};
+		ConfigMainCash.createODWithCashDocs(od_range);
 
 	}	
 	
